ba22d4a75d37ed4166cf79952faddc00ce6c05aa
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-06 16:47:24 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const input = document.querySelector('#searchByID').value
+        console.log(input)
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(response => response.json())
+        .then(movie =>{
+            document.querySelector('h4').textContent=movie.title
+            document.querySelector('p').textContent=movie.summary
+            console.log(movie)
+        })
+    })
 }
+
+
 
 document.addEventListener('DOMContentLoaded', init);
